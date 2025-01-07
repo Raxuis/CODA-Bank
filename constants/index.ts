b5d4cd1bdb826868a13f055778cb182a240de4b5
Raxuis <raclot.raphael@gmail.com>
2025-01-07 18:34:09 +0100

    feat(createAccount): add functions + constants to create account

--- constants/index.ts
+++ constants/index.ts
@@ -1,15 +1,24 @@
-type Menus = {
+import {createBankAccount} from "../src/utils/BankAccountController";
+
+type Menu = {
     title: string,
-    value: string,
+    value: string | number,
     action: Function
 }
 
-export const menus: Menus[] = [
+export const menus: Menu[] = [
     {
         title: "Créer un compte",
         value: "create",
+        action:
+            async () => {
+                await createBankAccount();
+            }
+    },
+    {
+        title: "Accéder au compte",
+        value: "read",
         action: () => {
-            console.log("test");
         }
     }
 ]
--- src/utils/BankAccountController.ts
+++ src/utils/BankAccountController.ts
@@ -3,16 +3,50 @@ import {v4 as uuidv4} from 'uuid';
 import {CLI} from "../CLI";
 import {BankAccount} from "../../models/BankAccount";
 
-async function askPin(): Promise<string> {
-    let pin: number = 0;
-    while (pin.toString().length != 4) {
-        pin = await CLI.askValue("Entrez votre code pin (longueur 4)", "number");
-    }
-    return bcrypt.hash(pin.toString(), 12);
+type PinAction = "login" | "register" | "logout";
+const pinRegex = /^\d{4}$/;
+let isCreatingAccount: boolean = false;
+
+async function askPin(action: PinAction): Promise<string> {
+    let pin: string = "";
+    const message = action === "register"
+        ? "Entrez un nouveau code PIN (4 chiffres) :"
+        : "Entrez votre code PIN (4 chiffres) :";
+    do {
+        pin = await CLI.askValue(message, "text");
+    } while (!pinRegex.test(pin))
+    return pin;
 }
 
-export async function createBankAccount() {
-    const hashedPin = await askPin();
-    const id = uuidv4();
-    console.log(new BankAccount(id, hashedPin));
+async function registerPin(): Promise<string> {
+    const pin = await askPin("register");
+    return bcrypt.hash(pin, 12);
 }
+
+// TODO: Compare pin entered and user pin
+// async function logIn(): Promise<Boolean> {
+//     const pin = await askPin("login");
+//     return bcrypt.compare(pin, )
+//
+// }
+
+export async function createBankAccount(): Promise<void> {
+    if (isCreatingAccount) return;
+    isCreatingAccount = true;
+    try {
+        const hashedPin = await registerPin();
+        const id = uuidv4();
+        const newAccount = new BankAccount(id, hashedPin);
+        console.log("Compte bancaire créé avec succès :", newAccount);
+    } catch (error) {
+        console.error("Erreur lors de la création du compte bancaire :", error);
+    } finally {
+        isCreatingAccount = false;
+    }
+}
+
+// export async function loginBankAccount() {
+//     try {
+//         const hashedPin = await logIn();
+//     }
+// }
