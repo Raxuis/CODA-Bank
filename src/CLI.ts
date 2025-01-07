86445d08a4ed1db258c6ab3dc72ac5b4638e620b
Raxuis <raclot.raphael@gmail.com>
2025-01-07 19:07:20 +0100

    refactor(CLI): change cli class logic to avoid issue when changing menus

--- src/CLI.ts
+++ src/CLI.ts
@@ -9,55 +9,39 @@ export interface CLIChoice extends Choice {
  * Represents a Command Line Interface (CLI) utility.
  */
 export class CLI {
-    /**
-     * An array of choices available in the CLI menu.
-     */
-    public choices: CLIChoice[] = [];
+    private readonly getMenus: () => CLIChoice[];
 
     /**
      * Creates an instance of the CLI class.
-     * @param choices - An array of CLIChoice objects to initialize the CLI with.
+     * @param getMenus - A function returning an array of CLIChoice objects.
      */
-    constructor(choices: CLIChoice[] = []) {
-        this.choices = choices;
+    constructor(getMenus: () => CLIChoice[]) {
+        this.getMenus = getMenus;
     }
 
     /**
      * Prompts the user to input a value.
-     * @param message - The message to display to the user.
-     * @param type - The type of input expected ("text" or "number").
-     * @returns A promise that resolves to the user's input, either a string or a number.
      */
-    public static async askValue(message: string, type: "text"): Promise<string>;
-    public static async askValue(
-        message: string,
-        type: "number"
-    ): Promise<number>;
-    public static async askValue(
-        message: string,
-        type: PromptType = "text"
-    ): Promise<string | number> {
+    public static async askValue(message: string, type: PromptType = "text"): Promise<string | number> {
         const response = await prompts({
             type,
             name: "value",
             message,
         });
-
         return response.value;
     }
 
     /**
      * Displays a menu to the user with the available choices.
-     * If a choice is selected, its action is executed.
-     * If "Quitter" is selected, calls the `quit` method.
      */
     public async menu() {
+        const choices = this.getMenus();
         const response = await prompts({
             type: "select",
             name: "action",
             message: "Que voulez-vous faire ?",
             choices: [
-                ...this.choices.map((choice) => ({
+                ...choices.map((choice) => ({
                     title: choice.title,
                     value: choice.value,
                 })),
@@ -65,9 +49,7 @@ export class CLI {
             ],
         });
 
-        const choice = this.choices.find(
-            (choice) => choice.value === response.action
-        );
+        const choice = choices.find((choice) => choice.value === response.action);
 
         if (choice) await choice.action();
         else await this.quit();
@@ -78,7 +60,6 @@ export class CLI {
 
     /**
      * Quit the CLI and exit the program.
-     * Waits for a random time between 0 and 2 seconds before exiting.
      */
     private async quit() {
         const randomTime = Math.floor(Math.random() * 2); // Random time between 0 and 2 seconds
